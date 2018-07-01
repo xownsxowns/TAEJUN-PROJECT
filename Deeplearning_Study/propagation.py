b6a1779c03d3c12326d9f33120d1dfe1b4507635
xownsxowns <xowns2794@gmail.com>
2018-07-02 02:02:54 +0900

    propagation

--- Deeplearning_Study/propagation.py
+++ Deeplearning_Study/propagation.py
@@ -1,2 +1,17 @@
-# XOR problem using backpropagation
+# Forward Propagation, Backpropagation
+# XOR problem
+import numpy as np
+
+x = np.array(([0,0],[1,0],[0,1],[1,1]), dtype=float)
+y = np.array(([0],[1],[1],[0]), dtype=float)
+
+class neural_network(object):
+    def __init__(self):
+        self.inputsize = 2
+        self.outputsize = 1
+        self.hiddensize = 2
+
+        # weight initialization
+        self.w1 = np.random.randn(self.inputsize, self.hiddensize)
+        self.w2 = np.random.randn(self.hiddensize, self.outputsize)
 
