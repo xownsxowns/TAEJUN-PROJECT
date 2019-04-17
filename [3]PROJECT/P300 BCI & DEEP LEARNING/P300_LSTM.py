78c3df8667b374bcf207dab330601ec26ceb2d1f
xownsxowns <xowns2794@gmail.com>
2019-04-17 20:57:47 +0900

    commit

---
+++ [3]PROJECT/P300 BCI & DEEP LEARNING/P300_LSTM.py
@@ -0,0 +1,103 @@
+
+## P300 Classification
+## LSTM
+
+# Epoch Sub1 ~ Sub30: TV
+# Epoch Sub31 ~ Sub45: Doorlock
+# Epoch Sub46 ~ Sub60: Lamp
+# Epoch BS Sub 1 ~Sub45: Bluetooth speaker
+
+# 1. Preprocessing
+#  1) 0.5Hz highpass filter (FIR)
+#  2) Bad channel rejection (1Hz lowpass filter , 2nd order Butter. , Corr. coeff < 0.4 , 70 % above)
+#  3) Common average re-reference
+#  4) 50Hz lowpass filter (FIR)
+#  5) Artifact subspace reconstruction (cutoff: 10)
+#
+# 2. Data
+#    ERP : [channel x time x stimulus type x block] (training: 50 block, test: 30 block)
+#    target : [block x 1] target stimulus of each block
+
+
+## validation, early stopping
+
+from scipy import io, signal
+import pandas as pd
+import numpy as np
+import random
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, LSTM, Conv2D, MaxPooling2D
+from sklearn.preprocessing import StandardScaler
+
+total_acc = list()
+
+for isub in range(60):
+    path = 'E:/[1] Experiment/[1] BCI/P300LSTM/Epoch_data/Epoch/Sub' + str(isub+1) + '_EP_training.mat'
+    data = io.loadmat(path)
+
+    nch = np.shape(data['ERP'])[0]
+    nlen = np.shape(data['ERP'])[1]
+    ntrain = np.shape(data['ERP'])[3]
+
+    tar_data = list()
+    tar_label = list()
+    nontar_data = list()
+    nontar_label = list()
+
+    for i in range(ntrain):
+        target = data['ERP'][:,:,data['target'][i][0]-1,i]
+        tar_data.append(target)
+        tar_label.append(1)
+
+        for j in range(4):
+            if j == (data['target'][i][0]-1):
+                continue
+            else:
+                nontar_data.append(data['ERP'][:,:,j,i])
+                nontar_label.append(0)
+
+    tar_data = np.reshape(tar_data,(ntrain,nlen,nch))
+    nontar_data = np.reshape(nontar_data,((ntrain*3),nlen,nch))
+
+    train_data = np.concatenate((tar_data, nontar_data))
+    train_label = np.concatenate((tar_label, nontar_label))
+
+    ## standardScaler 해줘보자
+    scalers = {}
+    for i in range(train_data.shape[1]):
+        scalers[i] = StandardScaler()
+        train_data[:, i, :] = scalers[i].fit_transform(train_data[:, i, :])
+
+    model = Sequential()
+    model.add(LSTM(20, input_shape=(nlen, nch), activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # print(model.summary())
+    model.fit(train_data, train_label, epochs=100, batch_size=20)
+
+    ## Test
+    path = 'E:/[1] Experiment/[1] BCI/P300LSTM/Epoch_data/Epoch/Sub' + str(isub+1) + '_EP_test.mat'
+    data2 = io.loadmat(path)
+    corr_ans = 0
+    ntest = np.shape(data2['ERP'])[3]
+
+    for i in range(ntest):
+        test = data2['ERP'][:,:,:,i]
+        total_prob = list()
+        for j in range(4):
+            test_data = test[:,:,j]
+            test_data = np.reshape(test_data, (1,nlen,nch))
+            for k in range(test_data.shape[1]):
+                test_data[:, k, :] = scalers[i].transform(test_data[:, k, :])
+            prob = model.predict_proba(test_data)
+            total_prob.append(prob[0][0])
+        predictedㅓㅕ_label = np.argmax(total_prob)
+        if data2['target'][i][0] == (predicted_label+1):
+            corr_ans += 1
+
+    total_acc.append((corr_ans/ntest)*100)
+    print("Accuracy: %.2f%%" % ((corr_ans/ntest)*100))
+    print(total_acc)
