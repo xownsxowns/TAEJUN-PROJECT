c108b7458f35ad443a54c6f0b0227be6243f2c57
xownsxowns <xowns2794@gmail.com>
2019-04-30 18:35:54 +0900

    commit

---
+++ [3]PROJECT/P300 BCI & DEEP LEARNING/P300_RCNN.py
@@ -0,0 +1,122 @@
+
+## P300 Classification
+## CNN
+
+# Epoch Sub1 ~ Sub30: TV
+# Epoch Sub31 ~ Sub45: Doorlock
+# Epoch Sub46 ~ Sub60: Lamp
+# Epoch BS Sub 1 ~Sub45: Bluetooth speaker
+
+# 1. Preprocessing
+#  1) 0.5Hz highpass filter (FIR)
+#  2) Bad channel rejection (1Hz lowpass filter , 2nd order Butter. , Corr. coeff < 0.4 , 70 % above)
+#  3) Common average re-reference
+#  4) 50Hz lowpass filter (FIR)
+#  5) Artifact subspace reconstruction (cutoff: 10)
+#
+# 2. Data
+#    ERP : [channel x time x stimulus type x block] (training: 50 block, test: 30 block)
+#    target : [block x 1] target stimulus of each block
+
+
+## validation, early stopping
+
+from scipy import io, signal
+import pandas as pd
+import numpy as np
+import random
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPooling1D, LSTM, SimpleRNN
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import csv
+
+total_acc = list()
+
+for isub in range(60):
+    path = 'E:/[1] Experiment/[1] BCI/P300LSTM/Epoch_data/Epoch/Sub' + str(isub+1) + '_EP_training.mat'
+    # path = '/Volumes/TAEJUN/[1] Experiment/[1] BCI/P300LSTM/Epoch_data/Epoch/Sub' + str(isub+1) + '_EP_training.mat'
+    data = io.loadmat(path)
+
+    nch = np.shape(data['ERP'])[0]
+    nlen = 200
+    ntrain = np.shape(data['ERP'])[3]
+
+    tar_data = list()
+    tar_label = list()
+    nontar_data = list()
+    nontar_label = list()
+
+    for i in range(ntrain):
+        target = data['ERP'][:,200:,data['target'][i][0]-1,i]
+        tar_data.append(target)
+        tar_label.append(1)
+
+        for j in range(4):
+            if j == (data['target'][i][0]-1):
+                continue
+            else:
+                nontar_data.append(data['ERP'][:,200:,j,i])
+                nontar_label.append(0)
+
+    tar_data = np.reshape(tar_data,(ntrain,nlen,nch))
+    nontar_data = np.reshape(nontar_data,((ntrain*3),nlen,nch))
+
+    train_vali_data = np.concatenate((tar_data, nontar_data))
+    train_vali_label = np.concatenate((tar_label, nontar_label))
+
+    train_data, vali_data, train_label, vali_label = train_test_split(train_vali_data, train_vali_label, test_size=0.15, random_state=42)
+
+    ## standardScaler 해줘보자
+    scalers = {}
+    for i in range(train_data.shape[1]):
+        scalers[i] = StandardScaler()
+        train_data[:, i, :] = scalers[i].fit_transform(train_data[:, i, :])
+        vali_data[:,i,:] = scalers[i].transform(vali_data[:,i,:])
+
+    model = Sequential()
+    model.add(Conv1D(filters=64, kernel_size=3 , input_shape=(nlen, nch), activation='relu', padding='same'))
+    # model.add(Conv1D(filters=64, kernel_size=3, activation='relu', padding='same'))
+    model.add(Dropout(0.25))
+    model.add(MaxPooling1D(pool_size=2))
+    # model.add(LSTM(50))
+    model.add(SimpleRNN(50))
+    # model.add(Flatten())
+    model.add(Dropout(0.25))
+    model.add(Dense(25, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # print(model.summary())
+    early_stopping = EarlyStopping(patience=10)
+    model.fit(train_data, train_label, epochs=200, batch_size=20, validation_data=(vali_data, vali_label), callbacks=[early_stopping])
+
+    ## Test
+    path = 'E:/[1] Experiment/[1] BCI/P300LSTM/Epoch_data/Epoch/Sub' + str(isub+1) + '_EP_test.mat'
+    # path = '/Volumes/TAEJUN/[1] Experiment/[1] BCI/P300LSTM/Epoch_data/Epoch/Sub' + str(isub+1) + '_EP_test.mat'
+    data2 = io.loadmat(path)
+    corr_ans = 0
+    ntest = np.shape(data2['ERP'])[3]
+
+    for i in range(ntest):
+        test = data2['ERP'][:,200:,:,i]
+        total_prob = list()
+        for j in range(4):
+            test_data = test[:,:,j]
+            test_data = np.reshape(test_data, (1,nlen,nch))
+            for k in range(test_data.shape[1]):
+                test_data[:, k, :] = scalers[i].transform(test_data[:, k, :])
+            prob = model.predict_proba(test_data)
+            total_prob.append(prob[0][0])
+        predicted_label = np.argmax(total_prob)
+        if data2['target'][i][0] == (predicted_label+1):
+            corr_ans += 1
+
+    total_acc.append((corr_ans/ntest)*100)
+    print("Accuracy: %.2f%%" % ((corr_ans/ntest)*100))
+    print(total_acc)
+
+df = pd.DataFrame(total_acc)
+filename = 'P300_RCNN Result_RNN.csv'
+df.to_csv(filename)
