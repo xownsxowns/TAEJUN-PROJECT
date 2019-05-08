52a1c7939783e29563ff07ae4f233a4e7827a1fc
xownsxowns <xowns2794@gmail.com>
2019-05-08 14:30:18 +0900

    commit

---
+++ [3]PROJECT/P300 BCI & DEEP LEARNING/EEG_to_image/P300_GAF.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+from pyts.image import GramianAngularField
+from scipy import io
+
+isub = 0
+path = 'E:/[1] Experiment/[1] BCI/P300LSTM/Epoch_data/Epoch/Sub' + str(isub+1) + '_EP_training.mat'
+data = io.loadmat(path)
+
+nch = np.shape(data['ERP'])[0]
+nlen = 200
+ntrain = np.shape(data['ERP'])[3]
+
+tar_data = list()
+tar_label = list()
+nontar_data = list()
+nontar_label = list()
+
+for i in range(ntrain):
+    target = data['ERP'][:, 200:, data['target'][i][0] - 1, i]
+    tar_data.append(target)
+    tar_label.append(1)
+
+    for j in range(4):
+        if j == (data['target'][i][0] - 1):
+            continue
+        else:
+            nontar_data.append(data['ERP'][:, 200:, j, i])
+            nontar_label.append(0)
+
+tar_data = np.reshape(tar_data, (ntrain, nlen, nch))
+nontar_data = np.reshape(nontar_data, ((ntrain * 3), nlen, nch))
+
+# Transform the time series into Gramian Angular Fields
+gasf = GramianAngularField(image_size=10, method='summation')
+X_gasf = gasf.fit_transform(tar_data[:,:,0])
+gadf = GramianAngularField(image_size=10, method='difference')
+X_gadf = gadf.fit_transform(tar_data[:,:,0])
+
+# images = [X_gasf[0], X_gadf[0]]
+images = [X_gasf[0]]
+titles = ['Gramian Angular Summation Field',
+          'Gramian Angular Difference Field']
+
+height = np.shape(images)[1]
+width = np.shape(images)[2]
+figsize = (height, width)
+# Show the images for the first time series
+# fig = plt.figure(figsize=(12, 7))
+fig = plt.figure(figsize=figsize)
+grid = ImageGrid(fig, 111,
+                 nrows_ncols=(1, 1)
+                 # axes_pad=0.15,
+                 # share_all=True,
+                 # cbar_location="right",
+                 # cbar_mode="single",
+                 # cbar_size="7%",
+                 # cbar_pad=0.3,
+                 )
+
+for image, title, ax in zip(images, titles, grid):
+    im = ax.imshow(image, cmap='rainbow', origin='lower')
+    # ax.set_title(title)
+# ax.cax.colorbar(im)
+# ax.cax.toggle_label(True)
+ax.axis('off')
+plt.xticks([]), plt.yticks([])
+plt.tight_layout()
+plt.subplots_adjust(bottom=0, top=1, right=1, left=0)
+plt.savefig("GAF.png",
+            bbox_inces='tight',
+            pad_inches=0)
+plt.show()
+
+
---
+++ [3]PROJECT/P300 BCI & DEEP LEARNING/EEG_to_image/make_frame.py
@@ -0,0 +1,97 @@
+# Plot
+import matplotlib
+import matplotlib.pyplot as plt
+import math
+
+# GIF
+from moviepy.editor import VideoClip
+from moviepy.video.io.bindings import mplfig_to_npimage
+
+from tools import create_time_serie
+import gramian_angular_field
+
+
+# Option to write gif
+write_gif = False
+
+# Instanciate figure
+fig = plt.figure(figsize=(9, 6))
+
+size = 0.33
+alignement = 0.1
+
+# Classic plot
+ax_carthesian = fig.add_axes([alignement, 0.4, size, size])
+# Polar plot
+ax_polar = fig.add_axes([alignement + size, 0.4, size, size], polar=True)
+# Patchwork
+ax_patchwork = fig.add_axes([alignement + 1.8*size, 0.4, size, size])
+
+# Global iteration
+iteration = 0
+
+# PLOTS
+# global size_time_serie
+# size_time_serie = float(45)
+
+def make_frame(time):
+    # Timesteps
+    global iteration
+
+    # Data
+    t, time_serie = create_time_serie(45, 0)
+    gaf, phi, r, scaled_time_serie = gramian_angular_field.GAF.transform(t, time_serie)
+
+    # Set to 0 unkown data at time stamp iteration
+    #t[iteration:] = 0
+    time_serie[iteration:] = 0
+    scaled_time_serie[iteration:] = 0
+    phi[iteration:] = math.acos(0)
+    #r[iteration:] = 0
+    gaf[iteration:, iteration:] = 0
+
+    # PLOTS
+    font = {
+        'family': 'serif',
+        'color':  'darkblue',
+        'weight': 'normal',
+        'size': 16,
+        }
+
+    # Clear plot
+    ax_carthesian.clear()
+    ax_polar.clear()
+    ax_patchwork.clear()
+
+    # Original Time series
+    ax_carthesian.plot(t, scaled_time_serie)
+    ax_carthesian.set_title("Scaled Time Serie", fontdict=font)
+    ax_carthesian.set_xticklabels([])
+
+    # Polar encoding
+    ax_polar.plot(phi, r)
+    ax_polar.set_title("Polar Encoding", fontdict=font)
+    ax_polar.set_rticks([0, 1])
+    ax_polar.set_rlabel_position(-22.5)
+    ax_polar.grid(True)
+
+    # Gramian Angular Field
+    ax_patchwork.matshow(gaf)
+    ax_patchwork.set_title("Gramian Angular Field", fontdict=font)
+    ax_patchwork.set_yticklabels([])
+    ax_patchwork.set_xticklabels([])
+
+    iteration = iteration + 1
+
+    return mplfig_to_npimage(fig)
+
+
+if __name__ == "__main__":
+
+    # GIF: Write and visualise
+    animation = VideoClip(make_frame, duration=2)
+
+    if(write_gif):
+        animation.write_gif("gramian_angulat_field.gif",fps=20)
+
+    animation.ipython_display(fps=20, loop=True, autoplay=True)
