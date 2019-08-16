d8a4889e51cf77b462b0ace93ebe0c51945f33f5
xownsxowns <xowns2794@gmail.com>
2019-08-16 20:23:28 +0900

    commit

--- [3]PROJECT/Face and object detection/FER_resnet.py
+++ [3]PROJECT/Face and object detection/FER_resnet.py
@@ -2,53 +2,215 @@ import pandas as pd
 import numpy as np
 import random
 from keras import optimizers
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation
+from keras import Input
+from keras.models import Model
+from keras.layers import BatchNormalization, Conv2D, Activation, Dense, GlobalAveragePooling2D, MaxPooling2D, ZeroPadding2D, Add
 
 ## CNN 모델로 해보자 유명한걸로
-# Initialize the CNN
-model = Sequential()
-# Step 1 - Convolution
-model.add(Conv2D(32, (3, 3), input_shape=(64,64,3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+# number of classes
+K = 7
 
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+input_tensor = Input(shape=(224, 224, 3), dtype='float32', name='input')
 
-model.add(Conv2D(128, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Conv2D(128, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
 
-model.add(Conv2D(256, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Conv2D(256, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-# Step 3 - Flattening
-model.add(Flatten())
-# Step 4 - Full connection
-model.add(Dense(128, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(7, activation='softmax'))
-print(model.summary())
+def conv1_layer(x):
+    x = ZeroPadding2D(padding=(3, 3))(x)
+    x = Conv2D(64, (7, 7), strides=(2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+
+    return x
+
+
+def conv2_layer(x):
+    x = MaxPooling2D((3, 3), 2)(x)
+
+    shortcut = x
+
+    for i in range(3):
+        if (i == 0):
+            x = Conv2D(64, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(64, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(256, (1, 1), strides=(1, 1), padding='valid')(x)
+            shortcut = Conv2D(256, (1, 1), strides=(1, 1), padding='valid')(shortcut)
+            x = BatchNormalization()(x)
+            shortcut = BatchNormalization()(shortcut)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+        else:
+            x = Conv2D(64, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(64, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(256, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+    return x
+
+
+def conv3_layer(x):
+    shortcut = x
+
+    for i in range(4):
+        if (i == 0):
+            x = Conv2D(128, (1, 1), strides=(2, 2), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(128, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(512, (1, 1), strides=(1, 1), padding='valid')(x)
+            shortcut = Conv2D(512, (1, 1), strides=(2, 2), padding='valid')(shortcut)
+            x = BatchNormalization()(x)
+            shortcut = BatchNormalization()(shortcut)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+        else:
+            x = Conv2D(128, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(128, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(512, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+    return x
+
+
+def conv4_layer(x):
+    shortcut = x
+
+    for i in range(6):
+        if (i == 0):
+            x = Conv2D(256, (1, 1), strides=(2, 2), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(256, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(1024, (1, 1), strides=(1, 1), padding='valid')(x)
+            shortcut = Conv2D(1024, (1, 1), strides=(2, 2), padding='valid')(shortcut)
+            x = BatchNormalization()(x)
+            shortcut = BatchNormalization()(shortcut)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+        else:
+            x = Conv2D(256, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(256, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(1024, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+    return x
+
+
+def conv5_layer(x):
+    shortcut = x
+
+    for i in range(3):
+        if (i == 0):
+            x = Conv2D(512, (1, 1), strides=(2, 2), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(512, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(2048, (1, 1), strides=(1, 1), padding='valid')(x)
+            shortcut = Conv2D(2048, (1, 1), strides=(2, 2), padding='valid')(shortcut)
+            x = BatchNormalization()(x)
+            shortcut = BatchNormalization()(shortcut)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+        else:
+            x = Conv2D(512, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(512, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(2048, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+    return x
+
+
+x = conv1_layer(input_tensor)
+x = conv2_layer(x)
+x = conv3_layer(x)
+x = conv4_layer(x)
+x = conv5_layer(x)
+
+x = GlobalAveragePooling2D()(x)
+output_tensor = Dense(K, activation='softmax')(x)
+
+resnet50 = Model(input_tensor, output_tensor)
+resnet50.summary()
+
 # Complie the CNN
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics= ['accuracy'])
+resnet50.compile(optimizer='adam', loss='categorical_crossentropy', metrics= ['accuracy'])
 
 # Fitting the CNN to the images
 # class 0:Anger, 1:Disgust, 2:Fear, 3:Happy, 4:Neutral, 5:Sad, 6:Surprise
@@ -58,20 +220,20 @@ train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory('E:/[2] 연구/[3] Facial/training_set',
-                                                 target_size=(64,64),
+                                                 target_size=(224,224),
                                                  batch_size=32,
                                                  class_mode='categorical')
 test_set = test_datagen.flow_from_directory('E:/[2] 연구/[3] Facial/test_set',
-                                            target_size=(64,64),
+                                            target_size=(224,224),
                                             batch_size=32,
                                             class_mode='categorical')
 
-model.fit_generator(training_set,
+resnet50.fit_generator(training_set,
                     steps_per_epoch=(280098//32),
                     epochs=50,
                     validation_data=test_set,
                     validation_steps=20)
 
 
-model.save("model2.h5")
+resnet50.save("model_resnet.h5")
 print("Saved model to disk")
