1b2d9e8fa395b68a3331c1391ba680deb45ba26b
xownsxowns <xowns2794@gmail.com>
2020-01-03 16:41:47 +0900

    commit

--- [3]PROJECT/P300 BCI & DEEP LEARNING/P300_FeatureExtraction/P300_rawfeature.py
+++ [3]PROJECT/P300 BCI & DEEP LEARNING/P300_FeatureExtraction/P300_rawfeature.py
@@ -53,7 +53,7 @@ for isub in range(30,60):
         baseline_mean = np.mean(baseline,axis=1,keepdims=True)
         baseline_std = np.std(baseline,axis=1,keepdims=True)
         target = data['ERP'][:,225:350,data['target'][i][0]-1,i]
-        target = (target-baseline_mean)/baseline_std
+        target = (target-baseline_mean)
         tar_data.append(target)
         tar_label.append(1)
 
@@ -62,7 +62,7 @@ for isub in range(30,60):
                 continue
             else:
                 nontar = data['ERP'][:,225:350,j,i]
-                nontar = (nontar-baseline_mean)/baseline_std
+                nontar = (nontar-baseline_mean)
                 nontar_data.append(nontar)
                 nontar_label.append(0)
 
@@ -74,33 +74,43 @@ for isub in range(30,60):
     # tar_data shape = (50, 125, 28)
     # ntar_data shape = (150, 125, 28)
     local_peak_tar = np.ones((np.shape(tar_data)[0],np.shape(tar_data)[2]))
+    local_peak_latency_tar = np.ones((np.shape(tar_data)[0],np.shape(tar_data)[2]))
+    mean_amp_tar = np.ones((np.shape(tar_data)[0],np.shape(tar_data)[2]))
     for ich in range(np.shape(tar_data)[2]):
         for itrial in range(np.shape(tar_data)[0]):
             sorted_data, sorted_index = np.sort(tar_data[itrial,:,ich])[::-1], np.argsort(-tar_data[itrial,:,ich])
             tar = tar_data[itrial,:,ich]
+            mean_amp_tar[itrial,ich] = np.mean(tar)
             for i in range(np.shape(tar_data)[1]):
                 if sorted_index[i] == np.shape(tar_data)[1]-1 or sorted_index[i] == 0:
                     continue
                 elif tar[sorted_index[i]] > tar[sorted_index[i]-1] and tar[sorted_index[i]] > tar[sorted_index[i]+1]:
                     local_peak_tar[itrial,ich] = tar[sorted_index[i]]
+                    local_peak_latency_tar[itrial,ich] = sorted_index[i]
                     break
 
     local_peak_ntar = np.ones((np.shape(nontar_data)[0],np.shape(nontar_data)[2]))
+    local_peak_latency_ntar = np.ones((np.shape(nontar_data)[0],np.shape(nontar_data)[2]))
+    mean_amp_ntar = np.ones((np.shape(nontar_data)[0],np.shape(nontar_data)[2]))
     for ich in range(np.shape(nontar_data)[2]):
         for itrial in range(np.shape(nontar_data)[0]):
             sorted_data, sorted_index = np.sort(nontar_data[itrial,:,ich])[::-1], np.argsort(-nontar_data[itrial,:,ich])
             ntar = nontar_data[itrial,:,ich]
+            mean_amp_ntar[itrial, ich] = np.mean(ntar)
             for i in range(np.shape(nontar_data)[1]):
                 if sorted_index[i] == np.shape(nontar_data)[1]-1 or sorted_index[i] == 0:
                     continue
                 elif ntar[sorted_index[i]] > ntar[sorted_index[i]-1] and ntar[sorted_index[i]] > ntar[sorted_index[i]+1]:
                     local_peak_ntar[itrial,ich] = ntar[sorted_index[i]]
+                    local_peak_latency_ntar[itrial,ich] = sorted_index[i]
                     break
 
-    train_data = np.concatenate((local_peak_tar, local_peak_ntar))
+    total_tar_data = np.concatenate((local_peak_tar, local_peak_latency_tar, mean_amp_tar), axis=1)
+    total_ntar_data = np.concatenate((local_peak_ntar, local_peak_latency_ntar, mean_amp_ntar), axis=1)
+    train_data = np.concatenate((total_tar_data, total_ntar_data))
     train_label = np.concatenate((tar_label, nontar_label))
 
-    clf = SVC(probability=True, kernel='sigmoid')
+    clf = SVC(probability=True)
     clf.fit(train_data, train_label)
 
     train_acc.append(clf.score(train_data, train_label))
@@ -118,25 +128,30 @@ for isub in range(30,60):
         baseline_test_mean = np.mean(baseline_test,axis=1,keepdims=True)
         baseline_test_std = np.std(baseline_test,axis=1,keepdims=True)
         test = data2['ERP'][:, 225:350, :, ii]
-        test = (test-baseline_test_mean)/baseline_test_std
+        test = (test-baseline_test_mean)
         total_prob = list()
         for j in range(4):
             test_data = test[:,:,j]
             test_data = np.reshape(test_data, (1,nlen,nch))
             local_peak_test = np.ones((4, np.shape(test_data)[2]))
+            local_peak_latency_test = np.ones((4, np.shape(test_data)[2]))
+            mean_amp_test = np.ones((4, np.shape(test_data)[2]))
             for ich in range(np.shape(test_data)[2]):
                 for itrial in range(np.shape(test_data)[0]):
                     sorted_data, sorted_index = np.sort(test_data[itrial, :, ich])[::-1], np.argsort(
                         -test_data[itrial, :, ich])
                     testt = test_data[itrial, :, ich]
+                    mean_amp_test[itrial,ich] = np.mean(testt)
                     for i in range(np.shape(test_data)[1]):
                         if sorted_index[i] == np.shape(test_data)[1] - 1 or sorted_index[i] == 0:
                             continue
                         elif testt[sorted_index[i]] > testt[sorted_index[i] - 1] and testt[sorted_index[i]] > testt[
                             sorted_index[i] + 1]:
                             local_peak_test[itrial, ich] = testt[sorted_index[i]]
+                            local_peak_latency_test[itrial, ich] = sorted_index[i]
                             break
-            prob = clf.predict_proba(local_peak_test)
+            total_test_data = np.concatenate((local_peak_test, local_peak_latency_test, mean_amp_test), axis=1)
+            prob = clf.predict_proba(total_test_data)
             total_prob.append(prob[0][0])
         predicted_label = np.argmin(total_prob)
         if data2['target'][ii][0] == (predicted_label+1):
@@ -169,7 +184,7 @@ for isub in range(14):
         baseline_mean = np.mean(baseline,axis=1,keepdims=True)
         baseline_std = np.std(baseline,axis=1,keepdims=True)
         target = data['ERP'][:,225:350,data['target'][i][0]-1,i]
-        target = (target-baseline_mean)/baseline_std
+        target = (target-baseline_mean)
         tar_data.append(target)
         tar_label.append(1)
 
@@ -178,7 +193,7 @@ for isub in range(14):
                 continue
             else:
                 nontar = data['ERP'][:,225:350,j,i]
-                nontar = (nontar-baseline_mean)/baseline_std
+                nontar = (nontar-baseline_mean)
                 nontar_data.append(nontar)
                 nontar_label.append(0)
 
@@ -190,35 +205,47 @@ for isub in range(14):
     # tar_data shape = (50, 125, 28)
     # ntar_data shape = (150, 125, 28)
     local_peak_tar = np.ones((np.shape(tar_data)[0],np.shape(tar_data)[2]))
+    local_peak_latency_tar = np.ones((np.shape(tar_data)[0],np.shape(tar_data)[2]))
+    mean_amp_tar = np.ones((np.shape(tar_data)[0],np.shape(tar_data)[2]))
     for ich in range(np.shape(tar_data)[2]):
         for itrial in range(np.shape(tar_data)[0]):
             sorted_data, sorted_index = np.sort(tar_data[itrial,:,ich])[::-1], np.argsort(-tar_data[itrial,:,ich])
             tar = tar_data[itrial,:,ich]
+            mean_amp_tar[itrial, ich] = np.mean(tar)
             for i in range(np.shape(tar_data)[1]):
                 if sorted_index[i] == np.shape(tar_data)[1]-1 or sorted_index[i] == 0:
                     continue
                 elif tar[sorted_index[i]] > tar[sorted_index[i]-1] and tar[sorted_index[i]] > tar[sorted_index[i]+1]:
                     local_peak_tar[itrial,ich] = tar[sorted_index[i]]
+                    local_peak_latency_tar[itrial, ich] = sorted_index[i]
                     break
 
     local_peak_ntar = np.ones((np.shape(nontar_data)[0],np.shape(nontar_data)[2]))
+    local_peak_latency_ntar = np.ones((np.shape(nontar_data)[0],np.shape(nontar_data)[2]))
+    mean_amp_ntar = np.ones((np.shape(nontar_data)[0],np.shape(nontar_data)[2]))
     for ich in range(np.shape(nontar_data)[2]):
         for itrial in range(np.shape(nontar_data)[0]):
             sorted_data, sorted_index = np.sort(nontar_data[itrial,:,ich])[::-1], np.argsort(-nontar_data[itrial,:,ich])
             ntar = nontar_data[itrial,:,ich]
+            mean_amp_ntar[itrial,ich] = np.mean(ntar)
             for i in range(np.shape(nontar_data)[1]):
                 if sorted_index[i] == np.shape(nontar_data)[1]-1 or sorted_index[i] == 0:
                     continue
                 elif ntar[sorted_index[i]] > ntar[sorted_index[i]-1] and ntar[sorted_index[i]] > ntar[sorted_index[i]+1]:
                     local_peak_ntar[itrial,ich] = ntar[sorted_index[i]]
+                    local_peak_latency_ntar[itrial,ich] = sorted_index[i]
                     break
 
-    train_data = np.concatenate((local_peak_tar, local_peak_ntar))
+    total_tar_data = np.concatenate((local_peak_tar, local_peak_latency_tar, mean_amp_tar), axis=1)
+    total_ntar_data = np.concatenate((local_peak_ntar, local_peak_latency_ntar, mean_amp_ntar), axis=1)
+    train_data = np.concatenate((total_tar_data, total_ntar_data))
     train_label = np.concatenate((tar_label, nontar_label))
 
-    clf = SVC(probability=True, kernel='sigmoid')
+    clf = SVC(probability=True)
     clf.fit(train_data, train_label)
 
+    train_acc.append(clf.score(train_data, train_label))
+    print(train_acc)
     ## Test
     # path = 'E:/[1] Experiment/[1] BCI/P300LSTM/Epoch_data/Epoch_BS/Sub' + str(isub+1) + '_EP_test.mat'
     # path = '/Users/Taejun/Desktop/현대실무연수자료/Epoch_BS/Sub' + str(isub + 1) + '_EP_test.mat'
@@ -232,25 +259,30 @@ for isub in range(14):
         baseline_test_mean = np.mean(baseline_test,axis=1,keepdims=True)
         baseline_test_std = np.std(baseline_test,axis=1,keepdims=True)
         test = data2['ERP'][:, 225:350, :, ii]
-        test = (test-baseline_test_mean)/baseline_test_std
+        test = (test-baseline_test_mean)
         total_prob = list()
         for j in range(6):
             test_data = test[:,:,j]
             test_data = np.reshape(test_data, (1,nlen,nch))
             local_peak_test = np.ones((6, np.shape(test_data)[2]))
+            local_peak_latency_test = np.ones((4, np.shape(test_data)[2]))
+            mean_amp_test = np.ones((4, np.shape(test_data)[2]))
             for ich in range(np.shape(test_data)[2]):
                 for itrial in range(np.shape(test_data)[0]):
                     sorted_data, sorted_index = np.sort(test_data[itrial, :, ich])[::-1], np.argsort(
                         -test_data[itrial, :, ich])
                     testt = test_data[itrial, :, ich]
+                    mean_amp_test[itrial, ich] = np.mean(testt)
                     for i in range(np.shape(test_data)[1]):
                         if sorted_index[i] == np.shape(test_data)[1] - 1 or sorted_index[i] == 0:
                             continue
                         elif testt[sorted_index[i]] > testt[sorted_index[i] - 1] and testt[sorted_index[i]] > testt[
                             sorted_index[i] + 1]:
                             local_peak_test[itrial, ich] = testt[sorted_index[i]]
+                            local_peak_latency_test[itrial, ich] = sorted_index[i]
                             break
-            prob = clf.predict_proba(local_peak_test)
+            total_test_data = np.concatenate((local_peak_test, local_peak_latency_test, mean_amp_test), axis=1)
+            prob = clf.predict_proba(total_test_data)
             total_prob.append(prob[0][0])
         predicted_label = np.argmin(total_prob)
         if data2['target'][ii][0] == (predicted_label+1):
@@ -261,5 +293,5 @@ for isub in range(14):
     print(total_acc)
 
 df = pd.DataFrame(total_acc)
-filename = 'P300_Result_SVM_peak.csv'
+filename = 'P300_Result_SVM_totalraw.csv'
 df.to_csv(filename)
