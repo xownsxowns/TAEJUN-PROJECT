d0172788129848d6c52c701968613842e99b920f
LEE TAEJUN <xowns2794@gmail.com>
2021-06-06 22:15:47 +0900

    commit

--- [2]STUDY/CV study/HandTrackingProject/HandTrackingMin.py
+++ [2]STUDY/CV study/HandTrackingProject/HandTrackingMin.py
@@ -23,7 +23,7 @@ while True:
                 # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
+                # print(id, cx, cy)
                 if id == 0:
                     cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
 
